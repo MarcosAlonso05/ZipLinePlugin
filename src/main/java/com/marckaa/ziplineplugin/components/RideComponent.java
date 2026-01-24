cd4c39685ed4b5db63a86fbbacb0f3e5ab51b4e9
Marcos Alonso <marcosalonsoamor05@gmail.com>
2026-01-25 00:34:20 +0100

    ;-;

---
+++ src/main/java/com/marckaa/ziplineplugin/components/RideComponent.java
@@ -0,0 +1,4 @@
+package com.marckaa.ziplineplugin.components;
+
+public class RideComponent {
+}
---
+++ src/main/java/com/marckaa/ziplineplugin/interactions/RideZipLineInteraction.java
@@ -0,0 +1,4 @@
+package com.marckaa.ziplineplugin.interactions;
+
+public class RideZipLineInteraction {
+}
